3c49723edd62e3c52a008d3323793a32a5a8bab9
Christopher Recher <chris@chrisrecher.com>
2014-08-11 23:00:19 -0700

    Lay groundwork for data access classes

---
+++ app/src/main/java/com/asusoda/taskdevil/data_access_layer/DataAccess.java
@@ -0,0 +1,23 @@
+package com.asusoda.taskdevil.data_access_layer;
+
+import com.asusoda.taskdevil.Task;
+
+import java.util.List;
+
+/**
+ * Created by root on 8/11/14.
+ */
+public class DataAccess {
+
+    public static boolean addOrUpdateTask(Task task){
+        return false;
+    }
+
+    public static boolean deleteTask(Task task){
+        return false;
+    }
+
+    public static Task getTask(){
+        return null;
+    }
+}
