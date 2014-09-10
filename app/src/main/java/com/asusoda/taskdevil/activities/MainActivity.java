b7f9cbc5bb3245874d5186bb941dbff9bab2b053
John Robertson <jjrob13@gmail.com>
2014-09-10 15:23:10 -0700

    Added onContextMenuItemSelected method to respond to context menu clicks

--- app/src/main/java/com/asusoda/taskdevil/activities/MainActivity.java
+++ app/src/main/java/com/asusoda/taskdevil/activities/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -142,6 +143,27 @@ public class MainActivity extends Activity {
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
         super.onCreateContextMenu(menu, v, menuInfo);
         getMenuInflater().inflate(R.menu.long_press_context, menu);
+    }
+
+
+    //determines what to do when a menu item in the context menu is selected
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.context_delete:
+                //where task will be deleted
+                //deleteTask(info.id);
+                Toast.makeText(getApplicationContext(), "NYI", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.context_edit:
+                //where a task will be edited
+                //editTask(info.id);
+                Toast.makeText(getApplicationContext(), "NYI", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 
 }
